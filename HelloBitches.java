56c5abe34798b75b96da7df57533175d13c2f8a9
Sayn-Da-Roy <73984789+Sayn-Da-Roy@users.noreply.github.com>
2021-01-06 12:05:01 +0530

    Create HelloBitches.java

---
+++ HelloBitches.java
@@ -0,0 +1,7 @@
+public class HelloBitches
+{
+    public static void main (String[] args)
+    {
+       System.out.println("Hello World (I know u read it lil Bitch!) (If you're offended you can suck my dick)");
+    }
+}
